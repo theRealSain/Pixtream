3e846ac1510bb2651f6cf833bfa96953208b4337
Sain Saburaj <sainsaburajpnc@gmail.com>
2024-07-31 18:14:47 +0530

    Second commit

--- home.php
+++ home.php
@@ -21,12 +21,12 @@ $info = mysqli_fetch_assoc($result);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>s-pixtream - Home</title>
+    <title>s-pixtream - Home Page</title>
 </head>
 <body>
-    <h1>Welcome <?php echo $username; ?></h1>
+    <h1>Welcome <?php echo $username; ?> to s-pixtream</h1>
 
-    Profile <br>
+    Profile Page<br>
     
     <?php
     echo "username: " . $username . "<br>";
